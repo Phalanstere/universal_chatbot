ef5ea10556abb26b39357ce81ab790def19def25
Phalanstere <burckhardt@ludicmedia.de>
2017-10-10 16:44:43 +0200

    new version

--- index.js
+++ index.js
@@ -23,25 +23,91 @@ var UniversalBot = function(params) {
 
     this.sessions = [];
 
+    this.change_topic = function(session) {
+                
+        var actual = session.conversation_state;
+        meta ("aktueller Status " + actual);
+        var x = self.strategy.chain.next( actual );
+
+        session.set_conversation_state(x.name);
+    }
+
+
+
+
     this.process_aiml = function(params) {
 
         if (params) {
             console.log("CALLBACK");
             console.log( params );
+            var s = self.find_session( params.session_id);
+            if (params.topic_change) self.change_topic( s );
+            else session.topic_counter ++;
+
+            // könnte die Repetition hier abfangen
+
         } else {
             meta("KEINE ANTWORT");
         }
-
-
-
 
         // console.log( params.answer.template );
 
     }
 
 
+    this.check_sessions = function() {
+
+    }
+
+
+    self.find_session = function(id) {
+        meta("ID : " + id);
+
+        for (var i = 0; i < self.sessions.length; i++) {
+            var ses = self.sessions[i];
+            if (ses.id === id) return ses;
+        }
+    return null;
+    }
+
+
+
+   
+
+    this.input = function(text, session_id, callback) {
+
+        meta("Hier kommt die Eingabe: " + text);
+        if (! session_id) {
+            meta("Neue Sitzung wird erzeugt ");
+            
+            var s = new Session(this, params.strategy);
+            
+            s.set_conversation_state( self.strategy.chain.nodes[0].name );
+
+            session_id = s.id;
+            self.sessions.push(s);
+            meta("Number of sessions " + self.sessions.length);
+        }
+
+        var session = self.find_session(session_id);
+
+
+
+        var input =  {
+            pattern: text,
+            topic: session.conversation_state.name
+        }
+
+        self.aiml.input (  input,  session, self.process_aiml);
+
+
+    }
+
+   
+
+
     this.init = function () {
-        self.session = new Session(this, params.strategy);
+        // self.session = new Session(this, params.strategy);
         
 
         if ( params.strategy) {
